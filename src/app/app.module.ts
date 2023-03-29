218be5949983717aa53d1c7d24af48df9b517a2e
lithdoh <lithifuse@gmail.com>
2023-03-28 18:47:42 -0700

    Add imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CheckboxesComponent } from './checkboxes/checkboxes.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CheckboxesNoExtraArrayComponent } from './checkboxes-no-extra-array/checkboxes-no-extra-array.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckboxesComponent,
+    CheckboxesNoExtraArrayComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatCheckboxModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
