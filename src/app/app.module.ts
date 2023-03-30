9a93a575fb83f21b3f878db83fb4624df814654d
lithdoh <lithifuse@gmail.com>
2023-03-30 09:59:22 -0700

    Add imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,23 +8,43 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CheckboxesNoExtraArrayComponent } from './checkboxes-no-extra-array/checkboxes-no-extra-array.component';
-
+import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { InMemoryCache } from '@apollo/client/core';
+import { CheckboxesAllCategoriesComponent } from './checkboxes-all-categories/checkboxes-all-categories.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CheckboxesComponent,
-    CheckboxesNoExtraArrayComponent
+    CheckboxesNoExtraArrayComponent,
+    CheckboxesAllCategoriesComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatExpansionModule,
     MatCheckboxModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    ApolloModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory(httpLink: HttpLink) {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'https://throbbing-field-240145.us-west-2.aws.cloud.dgraph.io/graphql',
+          }),
+        };
+      },
+      deps: [HttpLink],
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
