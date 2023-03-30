501e451731c9988bd7ad81980f4e0e005df883f9
lithdoh <lithifuse@gmail.com>
2023-03-30 09:59:06 -0700

    Add support for the checkboxes-all-categories component to load asynchronously

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, importProvidersFrom, inject } from '@angular/core';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-checkbox-form';
+  api = inject(ApiService);
 }
