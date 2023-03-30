f11c187bd8aec0f6f5bf09769dc8be5addfbda8d
lithdoh <lithifuse@gmail.com>
2023-03-30 10:00:07 -0700

    initial commit

---
+++ src/app/models/stem.ts
@@ -0,0 +1,14 @@
+export interface Stems {
+    link: string;
+    image: string;
+    name: string;
+    brand: string;
+    clampDiameter: number;
+    length: number;
+    rise: number;
+    steererDiameter: string;
+    color: string;
+    material: string;
+    price: number;
+    weight: number;
+  }  
