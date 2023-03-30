0c8dd103b6eedf32f78b95bcda7241684ce8ba2d
lithdoh <lithifuse@gmail.com>
2023-03-30 09:59:49 -0700

    Initial commit

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { Stems } from '../models/stem';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  baseURL =
+    'https://throbbing-field-240145.us-west-2.aws.cloud.dgraph.io/graphql?query=';
+  requestURL =
+    this.baseURL +
+    `{queryStems { brand color length material rise steererDiameter clampDiameter }}`;
+
+  stems$ = inject(HttpClient)
+    .get<StemResponse>(this.requestURL)
+    .pipe(map(({ data: { queryStems } }) => queryStems));
+}
+
+type StemResponse = {
+  data: {
+    queryStems: Stems[];
+  };
+
+}
+
